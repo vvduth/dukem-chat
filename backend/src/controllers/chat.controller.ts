07e8937269a7eae58888b513ff1aea4aecc1a776
vvduth <ducthai060501@gmail.com>
2026-01-08 17:32:57 +0200

    feat: refactor chat service and controller to consolidate chat creation and retrieval logic

--- backend/src/controllers/chat.controller.ts
+++ backend/src/controllers/chat.controller.ts
@@ -1,9 +1,8 @@
 import { Request, Response } from "express";
 import { asyncHandler } from "../middlewares/asyncHandler.middleware";
 import { HTTPSTATUS } from "../config/http.config";
-import { getUsersService } from "../services/user.service";
 import { createChatSchema } from "../validators/chat.validator";
-import { createChatService } from "../services/chat.service";
+import { createChatService, getUserChatsService } from "../services/chat.service";
 
 export const createChatController = asyncHandler(
   async (req: Request, res: Response) => {
@@ -14,9 +13,21 @@ export const createChatController = asyncHandler(
     const chat = await createChatService(userId, body);
 
     return res.status(HTTPSTATUS.OK).json({
-      success: true,
-      data: chat,
+      chat,
       message: "Chat created successfully",
     });
   }
 );
+
+
+export const getUserChatsController = asyncHandler(
+  async (req: Request, res: Response) => {
+    const userId = req.user?._id;
+    const chats = await getUserChatsService(userId);
+
+    return res.status(HTTPSTATUS.OK).json({
+      messages: "User chats fetched successfully",
+      chats,
+    });
+  }
+)
--- backend/src/services/chat.service.ts
+++ backend/src/services/chat.service.ts
@@ -1,25 +1,70 @@
+import ChatModel from "../models/chat.model";
+import UserModel from "../models/user.model";
+import { BadRequestException } from "../utils/app-error";
 
+export const createChatService = async (
+  userId: string,
+  body: {
+    participantId?: string;
+    isGroup?: boolean;
+    participants?: string[];
+    groupName?: string;
+  }
+) => {
+  const { participantId, isGroup, participants, groupName } = body;
 
-export const createChatService = async (userId: string ,
-    body: {
-        participantId?: string, 
-        isGroup?: boolean,
-        participants?: string[],
-        groupName?: string
+  let chat;
+  let allParticipantIds: string[] = [];
+
+  if (isGroup && participants?.length && groupName) {
+    allParticipantIds = [userId, ...participants];
+    chat = await ChatModel.create({
+      participants: allParticipantIds,
+      isGroup: true,
+      groupName,
+      createdBy: userId,
+    });
+  } else if (participantId) {
+    const otherUser = await UserModel.findById(participantId);
+    if (!otherUser) {
+      throw new BadRequestException("Participant user not found");
     }
-    
-) =>  {
-    
-    const {
-        participantId,
-        isGroup,
-        participants,
-        groupName
-    } = body
-    let chat;
-    let allParticipants: string[] = [];
-
-    if (isGroup && participants?.length && groupName) {
-        allParticipants = [userId,...participants];
+    allParticipantIds = [userId, participantId];
+    const existingChat = await ChatModel.findOne({
+      participants: {
+        $all: allParticipantIds,
+        $size: 2,
+      },
+    }).populate("participants", "name avatar");
+    if (existingChat) {
+      return existingChat;
     }
-}
+
+    chat = await ChatModel.create({
+      participants: allParticipantIds,
+      isGroup: false,
+      createdBy: userId,
+    });
+  }
+
+  // Implement websocket
+  return chat;
+};
+
+export const getUserChatsService = async (userId: string) => {
+  const chats = await ChatModel.find({
+    participants: {
+      $in: [userId],
+    },
+  })
+    .populate("participants", "name avatar")
+    .populate({
+      path: "lastMessage",
+      populate: {
+        path: "sender",
+        select: "name avatar",
+      }
+    })
+    .sort({ updatedAt: -1 });
+  return chats;
+};
